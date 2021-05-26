e87d38f9ad3a42e49255281af07b3c49f485e95d
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-05-26 16:05:13 -0300

    starting repository

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá mundo!"
+    });
+});
+
+app.post("/", (request, response) => {
+    return response.json({message: "Usuário salvo com sucesso!"})
+})
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
