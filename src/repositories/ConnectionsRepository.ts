dffd9389655e147923ffcc7070e910cf3c9c823c
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-05-28 18:06:52 -0300

    repositorie 'connections'

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,7 @@
+import { Repository } from "typeorm";
+
+import { Connection } from '../entities/Connection';
+
+class ConnectionRepository extends Repository<Connection> {}
+
+export { ConnectionRepository }
